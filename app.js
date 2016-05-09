373c4ed67062430dc509bcf3deff3e5d8a6b23f2
Daniel Humbert <achristian4truth@yahoo.com>
2016-05-09 17:07:51 -0500

    Fixes

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 /* app.js   thinkster tutorial */
 
 var app = angular.module('flapperNews', ['ui.router']);
-app.config([
-	'$stateProvider',
-	'urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider
 		.state( 'home', {
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
 		});
 
-	$urlRouterProvider.otherwise('home');
+		$urlRouterProvider.otherwise('home');
 }]);
+
+// Posts factory
+
 app.factory('posts', [function(){
 	var o = {
 		posts: []
 	};
 	return o;
 }]);
+
+// Main controller
+
 app.controller('MainCtrl', [
 	'$scope',
 	'posts',
@@ -28,24 +36,36 @@ app.controller('MainCtrl', [
 		$scope.test = 'Hello world!';
 
 		$scope.posts = posts.posts; [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4},
-	];
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 2},
+			{title: 'post 3', upvotes: 15},
+			{title: 'post 4', upvotes: 9},
+			{title: 'post 5', upvotes: 4},
+		];
+}]);
 
-	$scope.addPost = function(){
-		if(!$scope.title || $scope.title === '') { return; }
-		$scope.posts.push({
-			title: $scope.title,
-			link: $scope.link,
-			upvotes: 0});
-		});
-		$scope.title = '';
-		$scope.link = '';
-	};
-	$scope.incrementUpvotes = function(post) {
-		post.upvotes += 1;
-	}
-	}]);
+// Posts Controller
+
+app.controller('PostsCtrl', [
+		'$scope',
+		'$stateParams',
+		'posts',
+		function($scope, $stateParams, posts){
+			$scope.addPost = function(){
+				if(!$scope.title || $scope.title === '') { return; }
+				$scope.posts.push({
+					title: $scope.title,
+					link: $scope.link,
+					upvotes: 0})
+					comments: [
+						{author: 'Joe', body: 'Cool post!', upvores: 0},
+						{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+					]
+				});
+				$scope.title = '';
+				$scope.link = '';
+			};
+			$scope.incrementUpvotes = function(post) {
+				post.upvotes += 1;
+			}
+}]);
